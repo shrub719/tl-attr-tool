c5605697ae44a60e67709cea067a67da4950a60d
shrub <shrub719@gmail.com>
2026-05-01 12:19:20 +0100

    adjust bit shift

--- src/main.c
+++ src/main.c
@@ -4,11 +4,14 @@
 #include <stdint.h>
 
 long getBytes(char *str, size_t start, size_t len) {
-    char byteString[len*2];
-    long attr;
+    char byteString[2];
+    long attr = 0;
 
-    memcpy(byteString, str+start*2, len*2);
-    attr = strtol(byteString, NULL, 16);
+    for (int i = 0; i < len; i ++) {
+        char *byteStart = str + (start + i) * 2;
+        memcpy(byteString, byteStart, 2);
+        attr += strtol(byteString, NULL, 16) << ((len - i - 1) * 3);
+    }
 
     return attr;
 }
